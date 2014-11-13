6cb37810c8187506169a7845013182f3aacf0380
Sergii Mertsalov <sergmerts@gmail.com>
2014-11-13 10:51:33 -0800

    add express api routes

--- server.js
+++ server.js
@@ -72,6 +72,36 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
+
 mongoose.connect('localhost');
 
+app.get('/api/shows', function(req, res, next){
+  var query = Show.find();
+  if (req.query.genre) {
+      guery.where({ genre: req.query.genre });
+  } else if (req.query.alphabet) {
+      query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i')});
+  } else {
+      query.limit(12);
+  }
+  query.exec(function(err, shows){
+    if(err) return next(err);
+      res.send(shows);
+  });
+});
 
+app.get('/api/shows/:id', function(req, res, next){
+  Show.findById(req.params.id, function(err, show){
+    if (err) return next(err);
+      res.send(show);
+  });
+});
+
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, {message: err.message});
+});
