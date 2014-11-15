9296b838209fd0498dfd570eaaff9b46e0063bf1
Sergii Mertsalov <sergmerts@gmail.com>
2014-11-14 16:05:14 -0800

    subscribe and unsubscribe users to shows

--- server.js
+++ server.js
@@ -254,6 +254,29 @@ app.post('/auth/signup', function(req, res, next) {
   });
 });
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next){
+  Show.findById(req.body.showId, function(err, show){
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err){
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next){
+  Show.findById(req.body.showId, function(err, show){
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err){
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
 app.get('*', function(req, res) {
   res.redirect('/#' + req.originalUrl);
 });
