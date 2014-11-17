b4064726c26ca83264a62af58bdb1054bc76c9ef
Sergii Mertsalov <sergmerts@gmail.com>
2014-11-17 09:48:53 -0800

    connect database to mongolab

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ var _ = require('lodash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var agenda = require('agenda')({ db: { address: 'mongodb://Mind92:Golub1971@ds053080.mongolab.com:53080/showtrackr
+' } });
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 
@@ -150,7 +151,8 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
-mongoose.connect('localhost');
+mongoose.connect('mongodb://Mind92:Golub1971@ds053080.mongolab.com:53080/showtrackr
+');
 
 app.get('/api/shows', function(req, res, next){
   var query = Show.find();
