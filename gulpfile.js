b78096a69d76e05e61a3acd2bbb80bc2fea44a2c
Sergii Mertsalov <sergmerts@gmail.com>
2014-11-17 09:07:09 -0800

    cache angular templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
+var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('sass', function(){
   gulp.src('public/stylesheets/style.scss')
@@ -25,10 +29,16 @@ gulp.task('compress', function() {
     .pipe(gulp.dest('public'));
 });
 
+gulp.task('templates', function() {
+  gulp.src('public/views/**/*.html')
+    .pipe(templateCache({ root: 'views', module: 'MyApp' }))
+    .pipe(gulp.dest('public'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('public/stylesheets/*.scss', ['sass']);
   gulp.watch('public/views/**/*.html', ['templates']);
   gulp.watch(['public/**/*.js', '!public/app.min.js', '!public/templates.js', '!public/vendor'], ['compress']);
 });
 
-gulp.task('default', ['sass', 'compress', 'watch']);
+gulp.task('default', ['sass', 'compress', 'templates', 'watch']);
